756749b471b51c7c21c3701f333993c7985ec3e0
Zohaib Hamdule <zohaib2002@gmail.com>
2022-01-22 10:45:15 +0300

    Added dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 // Define Application
 const app = express();
